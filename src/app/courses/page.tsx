113de95da9b59dd543a8879e616103233fe87f9e
Mathias Oyono <oyonomathias@gmail.com>
2025-11-03 01:47:48 +0000

    Crée un fichier CSS3 pour la réactivité mobile de Formafrique : - Menu h

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import Image from 'next/image';
@@ -106,7 +107,7 @@ export default function CoursesPage() {
         </DropdownMenu>
       </div>
 
-      <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         {filteredCourses.map((course) => {
           const courseImage = PlaceHolderImages.find((img) => img.id === course.image);
           return (
