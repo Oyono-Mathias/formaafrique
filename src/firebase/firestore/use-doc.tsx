e6e292143e5e3f57169794c519b6755827301bf6
Mathias Oyono <oyonomathias@gmail.com>
2025-11-08 15:11:45 +0000

    FirebaseError: Function doc() cannot be called with an empty path.     a

--- src/firebase/firestore/use-doc.tsx
+++ src/firebase/firestore/use-doc.tsx
@@ -4,15 +4,16 @@ import { useState, useEffect } from 'react';
 import { doc, onSnapshot, DocumentData } from 'firebase/firestore';
 import { useFirestore } from '@/firebase/provider';
 
-export function useDoc<T>(collectionName: string, docId: string) {
+export function useDoc<T>(collectionName: string | null, docId: string | null) {
     const db = useFirestore();
     const [data, setData] = useState<T | null>(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<Error | null>(null);
 
     useEffect(() => {
-        if (!db || !docId) {
+        if (!db || !collectionName || !docId) {
             setLoading(false);
+            setData(null);
             return;
         }
 
