a19df82302f16f6f7b5ff78919034078a5a284e1
Mathias Oyono <oyonomathias@gmail.com>
2025-11-11 14:48:22 +0000

    export default function NotFound() {   return (     <main className="fle

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ export default function NotFound() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-red-600">404 - Page introuvable</h1>
-      <p className="text-gray-500 mt-2">Retournez à la page d’accueil pour continuer.</p>
+      <p className="text-gray-500 mt-3">Retournez à la page d’accueil.</p>
     </main>
   );
 }
